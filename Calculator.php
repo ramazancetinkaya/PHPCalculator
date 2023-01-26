e79ff52b86b8ee076c735979b57ddc96b112a9ec
Ramazan Çetinkaya <75214035+ramazancetinkaya@users.noreply.github.com>
2023-01-26 21:29:29 +0300

    Update Calculator.php

--- Calculator.php
+++ Calculator.php
@@ -1,112 +1,196 @@
 <?php
 
 class Calculator {
-    private float $result; // store the result of the calculation
-
-    // Add two numbers
-    public function add(float $a, float $b): float {
-        $this->result = $a + $b;
+    // Store the result of the last calculation
+    private $result;
+  
+    /**
+     * Add two numbers
+     *
+     * @param float $x
+     * @param float $y
+     * @return float
+     */
+    public function add(float $x, float $y): float {
+        $this->result = $x + $y;
         return $this->result;
     }
 
-    // Subtract two numbers
-    public function subtract(float $a, float $b): float {
-        $this->result = $a - $b;
+    /**
+     * Subtract two numbers
+     *
+     * @param float $x
+     * @param float $y
+     * @return float
+     */
+    public function subtract(float $x, float $y): float {
+        $this->result = $x - $y;
         return $this->result;
     }
 
-    // Multiply two numbers
-    public function multiply(float $a, float $b): float {
-        $this->result = $a * $b;
+    /**
+     * Multiply two numbers
+     *
+     * @param float $x
+     * @param float $y
+     * @return float
+     */
+    public function multiply(float $x, float $y): float {
+        $this->result = $x * $y;
         return $this->result;
     }
 
-    // Divide two numbers
-    public function divide(float $a, float $b): float {
-        if ($b == 0) {
-            throw new Exception("Cannot divide by zero");
-        }
-        $this->result = $a / $b;
+    /**
+     * Divide two numbers
+     *
+     * @param float $x
+     * @param float $y
+     * @return float
+     */
+    public function divide(float $x, float $y): float {
+        $this->result = $x / $y;
         return $this->result;
     }
 
-    //square function 
-    public function square(float $a): float {
-        $this->result = pow($a, 2);
-        return $this->result;
-    }
-    //square root function 
-    public function squareRoot(float $a): float {
-        if ($a < 0) {
-            throw new Exception("Cannot calculate square root of negative number");
-        }
-        $this->result = sqrt($a);
+    /**
+     * Calculate the square of a number
+     *
+     * @param float $x
+     * @return float
+     */
+    public function square(float $x): float {
+        $this->result = $x * $x;
         return $this->result;
     }
 
-    //power function
-    public function power(float $base, int $exponent): float {
-        $this->result = pow($base, $exponent);
+    /**
+     * Calculate the square root of a number
+     *
+     * @param float $x
+     * @return float
+     */
+    public function squareRoot(float $x): float {
+        $this->result = sqrt($x);
         return $this->result;
     }
 
-    //mod function
-    public function mod(float $a, float $b): float {
-        $this->result = fmod($a, $b);
+    /**
+     * Raise a number to a power
+     *
+     * @param float $x
+     * @param int $y
+     * @return float
+     */
+    public function power(float $x, int $y): float {
+        $this->result = pow($x, $y);
         return $this->result;
     }
 
-    //trigonometric functions
-    public function sin(float $a): float {
-        $this->result = sin($a);
-        return $this->result;
-    }
-    public function cos(float $a): float {
-        $this->result = cos($a);
-        return $this->result;
-    }
-    public function tan(float $a): float {
-        $this->result = tan($a);
-        return $this->result;
-    }
-
-    //logarithmic functions
-    public function logarithm(float $a, int $base): float {
-        if ($base <= 0) {
-            throw new Exception("Invalid base for logarithm");
-        }
-        $this->result = log($a, $base);
-        return $this->result;
-    }
-    public function naturalLogarithm(float $a): float {
-        $this->result = log($a);
+    /**
+     * Calculate the modulus of two numbers
+     *
+     * @param float $x
+     * @param float $y
+     * @return float
+     */
+    public function mod(float $x, float $y): float {
+        $this->result = $x % $y;
         return $this->result;
     }
 
-    //exponent function
-    public function exponent(float $a): float {
-        $this->result = exp($a);
+    /**
+     * Calculate the sine of an angle
+     *
+     * @param float $x angle in degree
+     * @return float
+     */
+    public function sin(float $x): float {
+        $this->result = sin(deg2rad($x));
         return $this->result;
     }
 
-    //factorial function
-    public function factorial(int $a): int {
-        if ($a < 0) {
-            throw new Exception("Cannot calculate factorial of negative number");
-        }
-        $this->result = $a;
-        for ($i = $a - 1; $i > 1; $i--) {
+    /**
+     * Calculate the cosine of an angle
+     *
+     * @param float $x angle in degree
+     * @return float
+     */
+    public function cos(float $x): float {
+        $this->result = cos(deg2rad($x));
+        return $this->result;
+    }
+
+    /**
+     * Calculate the tangent of an angle
+     *
+     * @param float $x angle in degree
+     * @return float
+     */
+    public function tan(float $x): float {
+        $this->result = tan(deg2rad($x));
+        return $this->result;
+    }
+
+    /**
+     * Calculate the logarithm of a number with a specified base
+     *
+     * @param float $x
+     * @param int $base
+     * @return float
+     */
+    public function logarithm(float $x, int $base): float {
+        $this->result = log($x, $base);
+        return $this->result;
+    }
+
+    /**
+     * Calculate the natural logarithm of a number
+     *
+     * @param float $x
+     * @return float
+     */
+    public function naturalLogarithm(float $x): float {
+        $this->result = log($x);
+        return $this->result;
+    }
+
+    /**
+     * Calculate the exponential function of a number
+     *
+     * @param float $x
+     * @return float
+     */
+    public function exponent(float $x): float {
+        $this->result = exp($x);
+        return $this->result;
+    }
+
+    /**
+     * Calculate the factorial of a number
+     *
+     * @param int $x
+     * @return int
+     */
+    public function factorial(int $x): int {
+        $this->result = 1;
+        for ($i = 1; $i <= $x; $i++) {
             $this->result *= $i;
         }
         return $this->result;
     }
 
-    // Get the result of the previous calculation
+    /**
+     * Get the result of the last calculation
+     *
+     * @return float
+     */
     public function getResult(): float {
         return $this->result;
     }
 
-    // Clear the result
+    /**
+     * Clear the stored result
+     */
     public function clearResult(): void {
         $this->result = 0;
     }
